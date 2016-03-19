efe4ca465395a3deebba6bb666511bf0f4e7651d
Kahfi Davy Busthomi <kahfidavyb@gmail.com>
2016-03-19 08:12:46 +0700

    Udah ada health
    Test

---
+++ Game1/Assets/SampleAssets/2D/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+	public Slider healthbar;
+
+	// Use this for initialization
+	void Start () {
+		healthbar.value = 200;
+		Debug.Log(""+healthbar.value);
+	}
+	void OnCollisionStay2D(Collision2D col){
+		if (col.gameObject.tag == "Dor") {
+			healthbar.value -= 1;
+			Debug.Log(""+healthbar.value);
+		}
+	//	if (healthbar.value < 1) {
+	//		Application.LoadLevel("Game Over");
+	//	}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
